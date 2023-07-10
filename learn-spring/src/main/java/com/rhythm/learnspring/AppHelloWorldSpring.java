8d22983e1e4601fdc77f7c55ac048b4e17f5ebf0
rhythmjayee <jayeerythm8@gmail.com>
2023-07-10 12:15:22 +0530

    record & spring beans

--- learn-spring/src/main/java/com/rhythm/learnspring/AppHelloWorldSpring.java
+++ learn-spring/src/main/java/com/rhythm/learnspring/AppHelloWorldSpring.java
@@ -15,8 +15,11 @@ public class AppHelloWorldSpring {
 		
 		//3: Retrieving Beans managed by Spring
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
   
 	}
 
 }
-   
+    
--- learn-spring/src/main/java/com/rhythm/learnspring/HelloWorldConfiguration.java
+++ learn-spring/src/main/java/com/rhythm/learnspring/HelloWorldConfiguration.java
@@ -3,11 +3,36 @@ package com.rhythm.learnspring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+//Eliminate verbosity in creating Java Beans
+//Public accessor methods, constructor, 
+//equals, hashcode and toString are automatically created. 
+//Released in JDK 16.
+
+record Person (String name, int age) { };
+
+//Address - firstLine & city
+record Address(String firstLine, String city){ };
+
 @Configuration
 public class HelloWorldConfiguration {
 //here we define spring beans(things managed by spring)
 	@Bean
 	public String name() {
 		return "Rhythm";
+	}
+	@Bean
+	public int age() {
+		return 15;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 20);		
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Baker Street", "London");		
 	}
 }
