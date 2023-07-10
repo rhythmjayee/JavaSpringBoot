aa3aaf424a7ce6c1cc5bb11b58b67337681ce4bd
rhythmjayee <jayeerythm8@gmail.com>
2023-07-10 12:27:12 +0530

    beans naming & relation

--- learn-spring/src/main/java/com/rhythm/learnspring/HelloWorldConfiguration.java
+++ learn-spring/src/main/java/com/rhythm/learnspring/HelloWorldConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 //equals, hashcode and toString are automatically created. 
 //Released in JDK 16.
 
-record Person (String name, int age) { };
+record Person (String name, int age, Address address) { };
 
 //Address - firstLine & city
 record Address(String firstLine, String city){ };
@@ -19,8 +19,9 @@ public class HelloWorldConfiguration {
 //here we define spring beans(things managed by spring)
 	@Bean
 	public String name() {
-		return "Rhythm";
+		return "Ranga";
 	}
+	
 	@Bean
 	public int age() {
 		return 15;
@@ -28,11 +29,27 @@ public class HelloWorldConfiguration {
 	
 	@Bean
 	public Person person() {
-		return new Person("Ravi", 20);		
+		return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));		
 	}
-	
+
 	@Bean
+	public Person person2MethodCall() {
+		return new Person(name(), age(), address()); //name, age		
+	}
+
+	@Bean
+	public Person person3Parameters(String name, int age, Address address3) {
+		//name,age,address3
+		return new Person(name, age, address3); //name, age		
+	}
+
+	@Bean(name = "address2")
 	public Address address() {
 		return new Address("Baker Street", "London");		
+	}
+
+	@Bean(name = "address3")
+	public Address address3() {
+		return new Address("Motinagar", "Hyderabad");		
 	}
 }
