d06649b000e9677244aa448f40f4dce781c126f7
rhythmjayee <jayeerythm8@gmail.com>
2023-07-10 22:04:36 +0530

    dependency injection types

---
+++ learn-spring/src/main/java/com/rhythm/learnspring/dependencyinjection/a1/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,75 @@
+package com.rhythm.learnspring.dependencyinjection.a1;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+@Component
+class YourBusinessClass {
+//	@Autowired -> dependency injection using fields
+	Dependency1 dependency1;
+//	@Autowired
+	Dependency2 dependency2;
+
+	@Autowired
+//	dependency injection using constructor - recommended
+	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+		super();
+		System.out.println("Constructor Injection - YourBusinessClass ");
+		this.dependency1 = dependency1;
+		this.dependency2 = dependency2;
+	}
+
+	//Setter based dependency injection
+//	@Autowired
+//	public void setDependency1(Dependency1 dependency1) {
+//		System.out.println("Setter Injection - setDependency1 ");
+//		this.dependency1 = dependency1;
+//	}
+//
+//	@Autowired
+//	public void setDependency2(Dependency2 dependency2) {
+//		System.out.println("Setter Injection - setDependency2 ");
+//		this.dependency2 = dependency2;
+//	}
+
+	public String toString() {
+		return "Using " + dependency1 + " and " + dependency2;
+	}
+
+}
+
+@Component
+class Dependency1 {
+
+}
+
+@Component
+class Dependency2 {
+
+}
+
+@Configuration
+@ComponentScan
+//auto wiring & creation of objects
+public class SimpleSpringContextLauncherApplication {
+
+	public static void main(String[] args) {
+
+		try (var context = 
+				new AnnotationConfigApplicationContext
+					(SimpleSpringContextLauncherApplication.class)) {
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			System.out.println(context.getBean(YourBusinessClass.class));
+
+		}
+	}
+
+}
