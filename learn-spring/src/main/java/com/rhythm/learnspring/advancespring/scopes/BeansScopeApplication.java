1e13c2013455c9189b347395c541e71a8c211ea8
rhythmjayee <jayeerythm8@gmail.com>
2023-07-17 10:06:37 +0530

    Spring beans scope

---
+++ learn-spring/src/main/java/com/rhythm/learnspring/advancespring/scopes/BeansScopeApplication.java
@@ -0,0 +1,53 @@
+package com.rhythm.learnspring.advancespring.scopes;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+
+@Component
+class NormalClass {
+	
+}
+
+//prototype scope -> will give new object reference whenever called
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass {
+	
+}
+
+
+@Configuration
+@ComponentScan
+//auto wiring & creation of objects
+public class BeansScopeApplication {
+
+	public static void main(String[] args) {
+
+		try (var context = 
+				new AnnotationConfigApplicationContext
+					(BeansScopeApplication.class)) {
+			
+			System.out.println("Initialization of context is completed");
+			
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			System.out.println(context.getBean(NormalClass.class));
+			
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+			System.out.println(context.getBean(PrototypeClass.class));
+
+		}
+	}
+
+}
