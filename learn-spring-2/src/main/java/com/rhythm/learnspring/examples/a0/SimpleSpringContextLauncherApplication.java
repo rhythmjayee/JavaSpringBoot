24af61b7612d6840d43d7bc687b5a3b0fb441205
rhythmjayee <jayeerythm8@gmail.com>
2023-07-17 12:08:02 +0530

    added learn-spring-2 folder

---
+++ learn-spring-2/src/main/java/com/rhythm/learnspring/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,27 @@
+package com.rhythm.learnspring.examples.a0;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+//auto wiring & creation of objects
+public class SimpleSpringContextLauncherApplication {
+
+	public static void main(String[] args) {
+
+		try (var context = 
+				new AnnotationConfigApplicationContext
+					(SimpleSpringContextLauncherApplication.class)) {
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+
+		}
+	}
+
+}
