baabb67d7f517b800e8f7b504045c71852c2dd7f
rhythmjayee <jayeerythm8@gmail.com>
2023-07-17 17:48:57 +0530

    XmlConfigurationContextLauncherApplication

---
+++ learn-spring-2/src/main/java/com/rhythm/learnspring/examples/java_xml_config/XmlConfigurationContextLauncherApplication.java
@@ -0,0 +1,28 @@
+package com.rhythm.learnspring.examples.java_xml_config;
+
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//@Configuration
+//@ComponentScan
+//auto wiring & creation of objects
+public class XmlConfigurationContextLauncherApplication {
+
+	public static void main(String[] args) {
+
+		try (var context = 
+				new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			System.out.println(context.getBean("name"));
+			
+			System.out.println(context.getBean("age"));
+			
+//			context.getBean(GameRunner.class).run();
+
+		}
+	}
+
+}
