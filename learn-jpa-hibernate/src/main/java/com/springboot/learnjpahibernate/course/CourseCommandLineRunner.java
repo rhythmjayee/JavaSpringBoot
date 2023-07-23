21bc39e75205f6ec704c91f9724e74e64aae068b
rhythmjayee <jayeerythm8@gmail.com>
2023-07-23 13:58:40 +0530

    spring data jpa setup

--- learn-jpa-hibernate/src/main/java/com/springboot/learnjpahibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-hibernate/src/main/java/com/springboot/learnjpahibernate/course/CourseCommandLineRunner.java
@@ -6,27 +6,43 @@ import org.springframework.stereotype.Component;
 
 import com.springboot.learnjpahibernate.course.jdbc.CourseJdbcRepository;
 import com.springboot.learnjpahibernate.course.jpa.CourseJpaRepository;
+import com.springboot.learnjpahibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
 
-	@Autowired
-	private CourseJdbcRepository repository;
+//	@Autowired
+//	private CourseJdbcRepository repository;
 	
 //	@Autowired
 //	private CourseJpaRepository repository;
 	
+	@Autowired
+	private CourseSpringDataJpaRepository repository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn AWS Jpa!", "in28minutes"));
-		repository.insert(new Course(2, "Learn Azure Jpa!", "in28minutes"));
-		repository.insert(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
+//		repository.insert(new Course(1, "Learn AWS Jpa!", "in28minutes"));
+//		repository.insert(new Course(2, "Learn Azure Jpa!", "in28minutes"));
+//		repository.insert(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
+		
+		repository.save(new Course(1, "Learn AWS Jpa!", "in28minutes"));
+		repository.save(new Course(2, "Learn Azure Jpa!", "in28minutes"));
+		repository.save(new Course(3, "Learn DevOps Jpa!", "in28minutes"));
 		
 		repository.deleteById(1l);
 		
 		System.out.println(repository.findById(2l));
 		System.out.println(repository.findById(3l));
+		
+		System.out.println(repository.findAll());
+		System.out.println(repository.count());
+		
+		System.out.println(repository.findByAuthor("in28minutes"));
+		System.out.println(repository.findByAuthor(""));
 
+		System.out.println(repository.findByName("Learn AWS Jpa!"));
+		System.out.println(repository.findByName("Learn DevOps Jpa!"));
 		
 	}
 
